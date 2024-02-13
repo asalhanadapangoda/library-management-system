deddd9c8966e24a095a7dade6f4bdc652074546b
asalhanadapangoda <asalhimsanda@gmail.com>
2024-02-13 21:53:27 +0530

    New Book

--- src/main/java/org/example/entity/BookEntity.java
+++ src/main/java/org/example/entity/BookEntity.java
@@ -4,13 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Generated;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Data
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
